8d4e9fd941b470339cf517d0022ffc2cc80f3526
Abhishek <amestri890@gmail.com>
2021-03-11 17:45:44 +0530

    Adding logo

--- src/components/Signup/BodySignup1/bodySignup1.js
+++ src/components/Signup/BodySignup1/bodySignup1.js
@@ -3,7 +3,10 @@ import Aux from '../../../HOC/aux'
 import Checkbox from '@material-ui/core/Checkbox';
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
-
+import { ReactComponent as Glogo } from '../../../assets/images/image.svg'
+import { ReactComponent as Facebooklogo } from '../../../assets/images/facebook.svg'
+import { ReactComponent as Applelogo } from '../../../assets/images/apple.svg'
+import { ReactComponent as Linkdinlogo } from '../../../assets/images/linkdin.svg'
 class BodySignup1 extends Component{
 
     componentDidMount(){
@@ -32,10 +35,18 @@ class BodySignup1 extends Component{
                         </p>
                     </div>
                     <div className="justify-self-center flex mt-10 max-w-xl ">
-                        <div className="m-5 w-9 h-9 bg-primary"></div>
-                        <div className="m-5 w-9 h-9 bg-primary"></div>
-                        <div className="m-5 w-9 h-9 bg-primary"></div>
-                        <div className="m-5 w-9 h-9 bg-primary"></div>
+                        <div className="m-5 w-12 h-12 p-1.5 border-sm border-gray-400">
+                            <Glogo/>
+                        </div>
+                        <div className="m-5 w-12 h-12 p-1.5 border-sm border-gray-400">
+                            <Facebooklogo/>
+                        </div>
+                        <div className="m-5 w-12 h-12 p-1.5 border-sm border-gray-400">
+                            <Applelogo/>
+                        </div>
+                        <div className="m-5 w-12 h-12 p-1.5 border-sm border-gray-400">
+                            <Linkdinlogo/>
+                        </div>
                     </div>
                     <Link to={{
                         pathname:'/signup2',
--- src/components/Signup/Success-tracker/tracker.js
+++ src/components/Signup/Success-tracker/tracker.js
@@ -7,16 +7,16 @@ class Tracker extends Component{
         return(
                 <div className={classes.tracker}>
                     <div className="grid justify-items-center absolute top-1/5 left-default w-20">
-                        <p className={`mb-5 ${this.props.track === 1 ? 'text-9xl' : 'text-5xl'} text-gradient bg-gradient-to-r from-red-600`}>1</p>
-                        <div className="mb-6 rounded-full h-2 w-2 flex bg-gradient-to-r from-red-500"></div>
-                        <div className="mb-2 rounded-full h-2 w-2 flex bg-gradient-to-r from-red-500"></div>
-                        <p className={`mb-5 ${this.props.track === 2 ? 'text-9xl' : 'text-5xl'} text-gradient bg-gradient-to-r from-red-600`}>2</p>
-                        <div className="mb-6 rounded-full h-2 w-2 flex bg-gradient-to-r from-red-500"></div>
-                        <div className="mb-2 rounded-full h-2 w-2 flex bg-gradient-to-r from-red-500"></div>
-                        <p className={`mb-5 ${this.props.track === 3 ? 'text-9xl' : 'text-5xl'} text-gradient bg-gradient-to-r from-red-600`}>3</p>
+                        <p className={`mb-5 ${this.props.track === 1 ? 'text-9xl' : 'text-5xl'} TrackerText`}>1</p>
+                        <div className="mb-6 rounded-full h-2 w-2 flex "></div>
+                        <div className="mb-2 rounded-full h-2 w-2 flex "></div>
+                        <p className={`mb-5 ${this.props.track === 2 ? 'text-9xl' : 'text-5xl'}`}>2</p>
+                        <div className="mb-6 rounded-full h-2 w-2 flex "></div>
+                        <div className="mb-2 rounded-full h-2 w-2 flex "></div>
+                        <p className={`mb-5 ${this.props.track === 3 ? 'text-9xl' : 'text-5xl'}`}>3</p>
                         <div className="mb-6 rounded-full h-2 w-2 flex bg-gradient-to-r from-red-400"></div>
                         <div className="mb-6 rounded-full h-2 w-2 flex bg-gradient-to-r from-red-400"></div>
-                        <p className={`mb-5 ${this.props.track === 4 ? 'text-9xl' : 'text-5xl'} text-gradient bg-gradient-to-r from-red-600`}>4</p>
+                        <p className={`mb-5 ${this.props.track === 4 ? 'text-9xl' : 'text-5xl'}`}>4</p>
                     </div>
                 </div>
         )
